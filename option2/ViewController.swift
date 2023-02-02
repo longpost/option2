1ddc0e0f7394645f8a7a55dbe030e97666a3315b
longpost <liyanbing1981@gmail.com>
2023-02-01 23:09:28 -0600

    Initial Commit

---
+++ option2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  option2
+//
+//  Created by Yanbing Li on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
